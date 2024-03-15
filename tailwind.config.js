79d3f4ee1c0a34112b1158daeab5ea6b64235748
mfathur <mfathur17@gmail.com>
2024-03-15 10:13:34 +0700

    Init tailwindcss and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        },
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["light"],
+                    primary: "#19C170",
+                    "primary-content": "white",
+                    "primary-focus": "mediumblue",
+                },
+            },
+        ],
+    },
+};
